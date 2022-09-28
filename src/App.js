90caa67257fb016a08f262fe04e5107bfb32e7d1
alejandro <aleblancogovantes@hotmail.com>
2022-09-28 19:05:17 +0200

    modified structure, introduces useState and UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import React from 'react'
+import MainPage from './pages/MainPage';
+import SlideDetails from './pages/SlideDetails';
+
+
 
 function App() {
+
+  const [slides, setSlides] = useState("")
+  const [thumbnail, setThumbnail] = useState("")
+
+  useEffect(()=> {
+    getSlides();
+    getThumbnail();
+  },[]);
+
+const getSlides = () => {
+  axios.get("https://imgmgt.api.preci.cloud/api/SlideImages")
+    .then((response) => {
+      setSlides(response.data)
+    })
+    .catch((err) => console.log("error geting slides",err))
+};
+
+const getThumbnail = () => {
+axios.get("https://wsi001.api.preci.cloud/api/WholeSlideImages/:id/Thumbnails/512x512.jpeg")
+  .then((response) => {
+    setThumbnail(response.data)
+  })
+  .catch((err) => console.log("error geting slides thumbnails",err))
+}
+
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+      <Routes>
+
+        <Route path='/'  element={<MainPage slides={slides}/>} />
+  
+        <Route path='/SlideImages/:slideId' element={<SlideDetails />} />
+      </Routes>
     </div>
   );
 }
